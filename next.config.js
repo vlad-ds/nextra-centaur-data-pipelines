98bb865c3a5d26f295b8bd865235bc4bca07825e
vladgheorghe <vlad.proex@gmail.com>
2024-12-31 12:31:41 +0100

    pre-render content

--- next.config.js
+++ next.config.js
@@ -2,15 +2,3 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
-
-module.exports = withNextra({
-  // Add API routes configuration
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ]
-  },
-})
